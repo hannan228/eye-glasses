69190b7e72090b03ec2f14ffd49995a46080c38b
hannan228 <abdulhannan1146@gmail.com>
2024-11-10 22:50:14 +0300

    Designed kedin and erkek page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,21 @@
 class DashboardsController < ApplicationController
 
   def index
-
   end
+
+  def kedin
+  end
+
+  def erkek
+  end
+
+  def blue_block
+  end
+
+  def sale
+  end
+
+  def kids
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home_pages#index"
 
-  resources :dashboards, only: [:index]
+  resources :dashboards, only: [:index] do
+    collection do
+      get :kedin
+      get :erkek
+      get :blue_block
+      get :sale
+      get :kids
+    end
+  end
+
+
 end
